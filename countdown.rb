263d61cd286b2b07dff59e0d9dbdfe9409c9fcc2
Daniel Lechok <dclechok@gmail.com>
2019-09-09 19:25:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,11 +5,11 @@ def countdown(num)
   while num > 0 do
     puts "#{num} SECOND(S)!"
     num -= 1
-    countdown_with_sleep(num)
+    countdown_with_sleep()
   end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(naptime)
+def countdown_with_sleep(naptime = nil)
   sleep(naptime)
 end
